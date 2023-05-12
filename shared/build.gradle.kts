3b07d657d7956530dee26b41e5612ac1b798686b
azmithabet <azmy-rady@elarabygroup.com>
2023-05-12 18:12:37 +0300

    add yarn.lock

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,8 +1,6 @@
 plugins {
     kotlin("multiplatform")
     id("com.android.library")
-    id("maven-publish")
-    id("signing")
 }
 
 kotlin {
@@ -47,80 +45,4 @@ android {
     defaultConfig {
         minSdk = 24
     }
-}
-
-
-
-group = "com.azmiradi.kmpaudioplayer"
-version = "0.1.2"
-
-apply(plugin = "maven-publish")
-apply(plugin = "signing")
-
-extensions.configure<PublishingExtension> {
-    repositories {
-        maven {
-            val isSnapshot = version.toString().endsWith("SNAPSHOT")
-            url = uri(
-                if (!isSnapshot) "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2"
-                else "https://s01.oss.sonatype.org/content/repositories/snapshots"
-            )
-
-            credentials {
-                username = System.getenv("OssrhUsername")
-                password = System.getenv("OssrhPassword")
-            }
-        }
-    }
-
-    val javadocJar = tasks.register<Jar>("javadocJar") {
-        archiveClassifier.set("javadoc")
-    }
-
-    publications {
-        withType<MavenPublication> {
-            artifact(javadocJar)
-
-            pom {
-                name.set("KMPAudioPlayer")
-                description.set("A Kotlin Multiplatform library for MediaPlayer")
-                licenses {
-                    license {
-                        name.set("Apache-2.0")
-                        url.set("https://opensource.org/licenses/Apache-2.0")
-                    }
-                }
-                url.set("https://github.com/azmiradi/KMPAudioPlayer")
-                issueManagement {
-                    system.set("Github")
-                    url.set("https://github.com/azmiradi/KMPAudioPlayer/issues")
-                }
-                scm {
-                    connection.set("https://github.com/azmiradi/KMPAudioPlayer.git")
-                    url.set("https://github.com/azmiradi/KMPAudioPlayer")
-                }
-                developers {
-                    developer {
-                        name.set("Azmi Radi")
-                        email.set("azmiradi97@gmail.com")
-                    }
-                }
-            }
-        }
-    }
-}
-
-val publishing = extensions.getByType<PublishingExtension>()
-extensions.configure<SigningExtension> {
-    useInMemoryPgpKeys(
-        System.getenv("SigningKeyId"),
-        System.getenv("SigningKey"),
-        System.getenv("SigningPassword"),
-    )
-
-    sign(publishing.publications)
-}
-
-project.tasks.withType(AbstractPublishToMaven::class.java).configureEach {
-    dependsOn(project.tasks.withType(Sign::class.java))
 }
